b8d7922d686b5719484c9c48e19ca76a2395d0f5
salah10-max <salaheddine.bachar@centrale-casablanca.ma>
2025-03-10 15:48:36 +0100

    Update calculator.py

--- exercise1/calculator.py
+++ exercise1/calculator.py
@@ -6,7 +6,7 @@ Number = Union[int, float]
 
 
 def add(a: Number, b: Number) -> Number:
-    """
+    "cette fonction prends comme arguments deux nombres et retourne leur somme qui est un nbre aussi""
     Return the sum of a and b.
 
     Args:
@@ -16,8 +16,10 @@ def add(a: Number, b: Number) -> Number:
     Returns:
         The sum of a and b
     """
-    # TODO: Implement this function
-    pass
+    
+    return a+b
+    
+    
 
 
 def subtract(a: Number, b: Number) -> Number:
@@ -31,7 +33,8 @@ def subtract(a: Number, b: Number) -> Number:
     Returns:
         The result of a - b
     """
-    # TODO: Implement this function
+    
+    return a-b
     pass
 
 
@@ -46,7 +49,8 @@ def multiply(a: Number, b: Number) -> Number:
     Returns:
         The product of a and b
     """
-    # TODO: Implement this function
+  
+    return a*b
     pass
 
 
@@ -64,5 +68,8 @@ def divide(a: Number, b: Number) -> Number:
     Raises:
         ValueError: If b is 0
     """
-    # TODO: Implement this function
+    if b=0:
+        raise ValueError("Division by zero is not allowed")  # Correction du retour d'erreur 
+    return a/b
+    
     pass
